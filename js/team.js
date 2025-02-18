c05446d290d5a9c7bcfa94973cfa5bee9f43b79d
Zen Path <zen.path@tuta.com>
2025-02-18 22:26:40 +0200

    refactor: update team class

--- js/team.js
+++ js/team.js
@@ -1,21 +1,53 @@
-class TeamLogo {
-    DEFAULT_LOGO_DIR = "../assets/images/teams/";
+import { PlayerCard } from "./player.js";
 
-    constructor(fileName, src = null) {
-        this.path = this.DEFAULT_LOGO_DIR + fileName;
-        this.src = src;
-    }
-}
-
-class Team {
-    COLORS = {
-        RED: "#d9042b",
-        BLUE: "#03658c",
+export class Team {
+    static LOGOS_DIR = "../assets/images/teams";
+    static SIDE = {
+        LEFT: "left",
+        RIGHT: "right",
     };
 
-    constructor(name, logo, color = null) {
+    static PLAYER_ORDER = {
+        MANUAL: 0,
+        BEST: 1,
+        WORST: 2,
+        RANDOM: 3,
+    };
+
+    constructor(name, logo, side = null, players = null) {
         this.name = name;
         this.logo = logo;
-        this.color = color;
+
+        this.side = side;
+        this.players = players;
+
+        this.positions = [];
+        this.winningChance = 50.0;
+
+        this.sidebarElem = null;
+    }
+
+    createSidebar() {
+        const sidebarElem = document.createElement("div");
+        sidebarElem.classList.add("full-cards-container", "hidden-scrollable");
+
+        sidebarElem.append(...this.getFullCards());
+        this.sidebarElem = sidebarElem;
+        return sidebarElem;
+    }
+
+    getFullCards() {
+        return this.players.map((player) => new PlayerCard(player).create());
+    }
+
+    // TODO: improve this logic
+    getMiniCards() {
+        return this.positions.map((player) => player.createMiniCard());
+    }
+
+    calcWinningChance() {
+        const winningChance = Math.random() * 100;
+        this.winningChance = winningChance;
+        return winningChance;
     }
 }
